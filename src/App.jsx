069d54f94194b2420fd86d1c346bd1bae7ee3df7
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-20 15:10:36 +0500

    установлен json server. сделан тестовый запрос

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useSelector } from "react-redux";
+import {useEffect} from 'react'
 import {TodoInput, TodoList, TodoListItem} from "./components/Todo";
+import axios from "axios";
 
 function App() {
   const todo = useSelector(state => state.todo)
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/posts')
+    .then((res) => console.log(res))
+  }, [])
 
   return (
     <TodoList>
